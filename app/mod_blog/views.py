d199a7ad3f2c48aae726ac4f4b6e01c6c4a3cca4
alive <alive@alive-Vostro-1550.(none)>
2017-11-16 00:14:23 +0530

    added blog crud

--- app/mod_blog/views.py
+++ app/mod_blog/views.py
@@ -7,16 +7,46 @@ from . import mod_blog
 from .forms import BlogForm
 
 
-@mod_blog.route('/addblog')
+@mod_blog.route('/addblog',methods=['GET','POST'])
 @login_required
 def add_blog():
 	blogform = BlogForm()
 
 	if blogform.validate_on_submit():
 		if blogform.publication_date.data:
-			blog = Blog(title=blogform.title.data,content=blogform.content.data,publication_date=blogform.publication_date.data)
+			blog = Blog(title=blogform.title.data,
+						content=blogform.content.data,
+						publication_date=blogform.publication_date.data,
+						author=current_user)
 		else:
-			blog = Blog(title=blogform.title.data,content=blogform.content.data)
+			blog = Blog(title=blogform.title.data,
+						content=blogform.content.data,
+						author=current_user)
+
+
+		db.session.add(blog)
+		db.session.commit()
+		flash('Added blog successfully')
+		return redirect(url_for('main.index'))	
 			
 	return render_template('addblog.html',form=blogform) 
+
+@mod_blog.route('/listblog')
+@login_required
+def list_blog():
+	import pdb
+	pdb.set_trace()
+	
+	blogs = current_user.blogs
+
+	return render_template('listblog.html',blogs=blogs)
+
+@mod_blog.route('/deleteblog/<int:id>')
+@login_required	
+def delete_blog(id):
+	blog=Blog.query.filter_by(id=id).first()
+	if blog:
+		db.session.delete(blog)
+		db.session.commit()
+		return redirect(url_for('.list_blog'))
 
